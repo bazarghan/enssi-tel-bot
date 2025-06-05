68c41cb44c51f164bec515a29342bd11f148dea0
alireza <bazarghan80@gmail.com>
2025-06-05 20:04:29 +0330

    feat: new fields for achievement

--- internal/models/achievement.go
+++ internal/models/achievement.go
@@ -14,4 +14,8 @@ type Achievement struct {
 	Type            string
 	ImageURL        string
 	MinWordRequired uint
+
+	TotalItems uint `gorm:"comment:Total items/pixels/blocks to unlock for this achievement"`
+	GridWidth  uint `gorm:"comment:For image grid achievements, the width of the grid in items/blocks"`
+	GridHeight uint `gorm:"comment:For image grid achievements, the height of the grid in items/blocks"`
 }
